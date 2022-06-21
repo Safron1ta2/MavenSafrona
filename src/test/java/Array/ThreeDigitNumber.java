7a5c2de969e622ad64127a7eadbf5379efc7257e
zulu <bushuychenko55@gmail.com>
2022-06-21 16:52:28 +0300

    UltraMegaCodeArrayUsing

--- src/test/java/Array/ThreeDigitNumber.java
+++ src/test/java/Array/ThreeDigitNumber.java
@@ -7,27 +7,19 @@ import java.util.Arrays;
 
 public class ThreeDigitNumber {
     public static void main(String[] Safronchik) {
-        int[] array = {1, 2, 3, 4};
-        Arrays.sort(array);
-        for (int i = 0; i < 2; i++) {
-            for (int j = 0; j < array.length - 1; j++) {
-                int random1 = (int) Math.floor(Math.random() * array.length);
-
-                for (int l = 0; l < array.length - 1; l++) {
-                    int random2 = (int) Math.floor(Math.random() * array.length);
-
-                    for (int o = 0; o < array.length - 1; o++) {
-                        int random3 = (int) Math.floor(Math.random() * array.length);
-                        char c1 = Character.forDigit(random1, 10);
-                        System.out.println(c1);
-                        char c2 = Character.forDigit(random2, 10);
-                        char c3 = Character.forDigit(random3, 10);
-                            System.out.println(c1+c2+c3);
+        int[] array = {1, 2, 3, 4, 5, 6};
+        int amount = 0;
+            for(int i:array) {
+                for (int j:array) {
+                    for (int k:array) {
+                        if(k != i && k != j && i != j){
+                            System.out.println(i + "" + j + "" + k);
+                            amount++;
+                        }
                     }
-
                 }
             }
-        }
+        System.out.println(amount);
     }
     @Test
     public void f(){
